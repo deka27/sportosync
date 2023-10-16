c6d2bebb24f34706f702cb24399e7ee6c28e933b
Kaustav Deka <116963260+deka27@users.noreply.github.com>
2023-10-16 23:23:36 +0530

    +cookie change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from './(components)/Header'
 import Footer from './(components)/Footer'
 import Line from './(components)/Line'
 
+export const dynamic = 'force-dynamic'
 
 const poppins = Poppins({ subsets: ['latin'], weight:['100','400','700'] })
 
